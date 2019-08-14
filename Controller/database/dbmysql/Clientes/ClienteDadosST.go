750da8d89d4455518aba482ab0e2fb4bf2f010d9
eliolima <elio.lima@grupomalta.com.br>
2019-08-14 15:00:21 -0300

    Add - API - DB Clientes

---
+++ Controller/database/dbmysql/Clientes/ClienteDadosST.go
@@ -0,0 +1,25 @@
+package Clientes
+
+import "time"
+
+type ClienteDadosST struct {
+	Id              int64     `db_autprimary:"true"`
+	EmpresaID       int64     `db_notnull:"true" db_unsigned:"true"`
+	DataCadastro    time.Time `db_notnull:"true"`
+	DataAtualizacao time.Time `db_notnull:"true"`
+	Nome            string    `db_notnull:"true"`
+	Email           string    `db_notnull:"true"`
+	TipoPessoaID    int64     `db_comm:"Tipo de pessoa ID - 0 Fisica, 1 juridica" db_default:"0"`
+	TipoPessoaDesc  string    `db_comm:"Tipo de pessoa ID - 0 Fisica, 1 juridica" db_default:"Pessoa Física"`
+	Doc1            string    `db_comm:"CNPJ ou CPF"`
+	Doc2            string    `db_comm:"IE ou RG"`
+	Ativado         bool      `db_default:"0" db_comm:"Ativação por email"`
+	DataAtivacao    time.Time
+	CategoriaID     int64  `db_comm:"Código da Categoria" db_default:"0"`
+	CategoriaDesc   string `db_comm:"Descrição da Categoria" db_default:"Definir"`
+}
+
+func NewClienteDadosST() *ClienteDadosST {
+	s := &ClienteDadosST{}
+	return s
+}
